99c5394ca91ba8f994ff2601ebcd9bb05f70a3e8
ferradjielie <ferradji.elie@outlook.fr>
2025-04-23 12:14:57 +0200

    create pages folder and Main.jsx, Main.modules.css files for home page for PWA Ballade application

---
+++ pwa-ballade-front-end/src/pages/Main/Main.jsx
@@ -0,0 +1,34 @@
+ 
+import styles from "./Main.modules.css"
+import balladeLogo from '../assets/icons/ballade-logo.png';
+import home from '../assets/icons/home-icon.png';
+import login from '../assets/icons/login-icon.png';
+import globe from '../assets/icons/globe-logo.png';
+import banniereBallade from '../assets/icons/banniere-ballade.jpg';
+import balladeSponsor from '../assets/icons/ballade-sponsor.png';
+import globe from '../assets/icons/globe-logo.png';
+
+
+
+ function Main() {
+     <div> 
+      <nav className={styles.navContent}>
+        <h1> <img src={balladeLogo} alt="Logo de Ballade" />   Association Ballade </h1>
+
+        <ul> 
+            <li> <img src={home} alt="home icon"/> </li>
+            <li> <img src={globe} alt="globe icon"/> </li>
+            <li> <img src={login} alt="login icon"/> </li>
+        </ul>
+      
+      </nav>
+     
+     <main>
+      
+
+     </main>
+    
+     </div>
+ }
+
+ export default Main 
