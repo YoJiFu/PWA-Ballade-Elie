905caa9505315b0123b4e83800dfbfdd37f01538
ferradjielie <ferradji.elie@outlook.fr>
2025-04-25 15:53:23 +0200

    feat: add copyright in footer, display current app version (v0.1.0), and add "Carte des morceaux" button

--- pwa-ballade-front-end/src/pages/Main/Main.jsx
+++ pwa-ballade-front-end/src/pages/Main/Main.jsx
@@ -12,56 +12,54 @@ import facebook from "../../assets/icons/facebook.png";
 import instagram from "../../assets/icons/instagram.png";
 import youtube from "../../assets/icons/youtube.png";
 import music from "../../assets/icons/music.png";
+import map from "../../assets/icons/map.png";
 
+function HomePage() {
+  return (
+    <div className={styles.pageWrapper}>
+      <header className={styles.headerContent}>
+        <div className={styles.loginIcon}>
+          <img src={login} alt="login icon" />
+        </div>
 
+        <ul>
+          <li>
+            <img className={styles.balladeLogo} src={balladeLogo} alt="Ballade logo" />
+          </li>
+          <h1 className={styles.balladeTitle}>Association Ballade</h1>
 
+          <li className={styles.musicCard}>
+            <h2 className={styles.cardTitle}>Morceaux</h2>
+            <img className={styles.musicIcon} src={music} alt="music icon" />
+          </li>
 
+          <li className={styles.mapCard}>
+            <h2 className={styles.mapTitle}>Carte des morceaux</h2>
+            <img className={styles.mapIcon} src={map} alt="map icon" />
+          </li>
+        </ul>
+      </header>
 
- function HomePage() {
+      <main>
+        <div className={styles.balladeContent}>
+          <img src={banniereBallade} alt="banniere Ballade" />
+          <img src={balladeSponsor} alt="sponsors Ballade" />
+        </div>
+      </main>
 
-  return ( 
-      <div className={styles.pageWrapper}> 
-       <header className={styles.headerContent}>
-                 
-                 <div className={styles.loginIcon}> <img src={login} alt="login icon" /></div>
-               
-               <ul>
-                  <li> <img className={styles.balladeLogo} src={balladeLogo} alt="Ballade logo"/> </li>
-                  <li className={styles.musicCard}>
-                      <h2 className={styles.cardTitle}>Liste des morceaux</h2>
-                         <img className={styles.musicIcone} src={music} alt="music icon" />
-                   </li>
-               </ul>
+      <footer className={styles.footerContent}>
+        <ul className={styles.iconsContent}>
+          <li><img src={youtube} alt="youtube icon" /></li>
+          <li><img src={facebook} alt="facebook icon" /></li>
+          <li><img src={instagram} alt="instagram icon" /></li>
+          <li><img src={book} alt="book icon" /></li>
+        </ul>
 
+        <div className={styles.copyrightBallade}>© Association Ballade</div>
+        <div className={styles.versionNumber}>v0.1.0</div>
+      </footer>
+    </div>
+  );
+}
 
-       </header>
-     
-     <main>
-
-      <div className={styles.balladeContent}>
-            <img src={banniereBallade} alt="banniere Ballade" />
-            <img src={balladeSponsor} alt="sponsors Ballade" />
-
-
-
-      </div>
-      
-
-     </main>
-
-     <footer className={styles.footerContent}>
-      <ul className={styles.iconsContent}>
-            <li>< img src={youtube} alt="youtube icon" /> </li>
-            <li> <img src={facebook} alt="facebook icon" /> </li>
-            <li> <img src={instagram} alt="instagram icon" /> </li>
-            <li> <img src={book} alt="book icon" /> </li>
-      </ul>
-
-     </footer>
-    
-     </div>
-     )
- }
-
-
- export default HomePage 
+export default HomePage;
