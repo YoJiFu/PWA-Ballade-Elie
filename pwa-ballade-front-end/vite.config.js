8db7df00aee29875d038760fab4591bf83889062
ferradjielie <ferradji.elie@outlook.fr>
2025-05-26 12:27:55 +0200

    commit

--- pwa-ballade-front-end/vite.config.js
+++ pwa-ballade-front-end/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-import { VitePWA } from 'vite-plugin-pwa/dist/index.mjs'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
